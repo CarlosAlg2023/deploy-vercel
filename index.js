22ded0488f2d39b56b07fe7380f6e6e2f0857da7
Carlos <cuenta0estable@gmail.com>
2024-01-07 08:57:01 -0500

    0.0

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-// Importa el módulo express
 const express = require('express');
-
-// Crea una instancia de la aplicación Express
 const app = express();
 const port = process.env.PORT || 3001;
 
-// Define una ruta de ejemplo
 app.get('/', (req, res) => {
-  res.send('Hola, este es un servidor Express!');
+  res.send('Hola, este es un servidor Express! jfjsjfbbfb');
 });
 
-// Inicia el servidor en el puerto especificado
 app.listen(port, () => {
   console.log(`Servidor Express escuchando en el puerto ${port}`);
 });
